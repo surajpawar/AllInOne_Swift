297c6bd317a0a3724a5a257b95cae7876bdcf6bb
Suraj Pawar <>
2018-05-29 18:51:41 +0530

    Project Strructure

--- AllInOne_Swift/2.App Design/IOS Design.swift
+++ AllInOne_Swift/2.App Design/IOS Design.swift
@@ -18,22 +18,102 @@ import Foundation
  
  
  
+ 
+// ----------------------------------------------------------------------------
 *** Adaptive Layout ***
  
  
+ 
+// ----------------------------------------------------------------------------
 *** Size Classes ***
  
  
+ 
+// ----------------------------------------------------------------------------
 *** Auto Resizing ***
  
+
  
+ 
+// ----------------------------------------------------------------------------
 *** Constraints ***
  
+
  
+// ----------------------------------------------------------------------------
 *** Storyboard and XIB ***
  
  
+ 
+ 
+// ----------------------------------------------------------------------------
 *** Constraints using code ***
 
+ 
+ 
+ 
+ 
+ 
+ // ----------------------------------------------------------------------------
+ *** Traits ***
+ 
+ 
+ 
+ 
+ // ----------------------------------------------------------------------------
+ *** Safe Area ***
+ 
+ 
+ 
+ 
+ // ----------------------------------------------------------------------------
+ *** Constrain to margin ***
+ 
+ Note: When you appling constraints to second superview then use safe area constraints so our background colour will not apply to top status bar.
+ 
+ 
+ 
+ 
+ // ----------------------------------------------------------------------------
+ *** Layout Margins ***
+ 
+ - Default
+
+
+ - Language directional
+
+
+ - Fixed
+ 
+ - Preserve Superview Margins
+ 
+ - Follow Readable Width
+ 
+ - Safe Area Relative Margins
+ 
+ - Sage Area Layout Guide
+ 
+ 
+ // ----------------------------------------------------------------------------
+ *** Alignment constrains ***
+ 
+ 
+ -Leading Edges
+ 
+ -Trailing Edges
+ 
+ -Top Edges
+ 
+ -Bottom Edges
+ 
+ -Horizontal Centers
+ 
+ -Vertical Centers
+ 
+ -Baseline
+ 
+ - Horizontally in Container
+ 
+ - Vertically in Container
  
 */
