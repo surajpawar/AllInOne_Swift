469d996d5091d879a8d174e8ca9411506e3981e3
Suraj Pawar <>
2018-05-29 16:29:49 +0530

    The Basics

---
+++ AllInOne_Swift/2.App Design/IOS Design.swift
@@ -0,0 +1,39 @@
+//
+//  IOS Design.swift
+//  AllInOne_Swift
+//
+//  Created by MAC7 on 29/05/18.
+//  Copyright © 2018 Suraj Pawar. All rights reserved.
+//
+
+import Foundation
+
+
+// ----------------------------------------------------------------------------
+
+/* *** IOS Design Concepts  ***
+    
+    
+*** Auto Layout ***
+ 
+ 
+ 
+*** Adaptive Layout ***
+ 
+ 
+*** Size Classes ***
+ 
+ 
+*** Auto Resizing ***
+ 
+ 
+*** Constraints ***
+ 
+ 
+*** Storyboard and XIB ***
+ 
+ 
+*** Constraints using code ***
+
+ 
+*/
--- AllInOne_Swift/4.Swift Theory/The Basics/TheBasics.swift
+++ AllInOne_Swift/4.Swift Theory/The Basics/TheBasics.swift
@@ -10,6 +10,49 @@ import Foundation
 
 // ----------------------------------------------------------------------------
 
+/* *** The Basic ***
+ 
+ Swift is a new programming language for 'iOS', 'macOS', 'watchOS', and 'tvOS' app development. Nonetheless, many parts of Swift will be familiar from your experience of developing in C and Objective-C.
+ 
+ Swift provides its own versions of all fundamental 'C' and 'Objective-C' types, including
+    'Int' for 'integers',
+    'Double' and 'Float' for floating-point values,
+    'Bool' for 'Boolean values', and
+    'String' for textual data.
+ 
+ Swift also provides powerful versions of the three primary collection types,
+    'Array',
+    'Set', and
+    'Dictionary',
+ as described in Collection Types.
+
+ 
+ In addition to familiar types, Swift introduces advanced types not found in Objective-C, such as "tuples".
+ 
+ 
+ ******** Tuples *********
+ 
+ "Tuples enable you to create and pass around groupings of values. You can use a tuple to return multiple values from a function as a single compound value."
+ 
+ 
+ ******** Optionals *********
+ 
+ Swift also introduces optional types, which handle the absence of a value. Optionals say either “there is a value, and it equals x” or “there isn’t a value at all”. Using optionals is similar to using nil with pointers in Objective-C, but they work for any type, not just classes. Not only are optionals safer and more expressive than nil pointers in Objective-C, they’re at the heart of many of Swift’s most powerful features.
+ 
+ 
+ ******** type-safe language *********
+ 
+ Swift is a type-safe language, which means the language helps you to be clear about the types of values your code can work with. If part of your code requires a String, type safety prevents you from passing it an Int by mistake. Likewise, type safety prevents you from accidentally passing an optional String to a piece of code that requires a nonoptional String. Type safety helps you catch and fix errors as early as possible in the development process.
+ 
+ 
+ */
+
+
+
+
+
+// ----------------------------------------------------------------------------
+
 /* *** Constants and Variables ***
 
 
